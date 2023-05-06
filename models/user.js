ea5e14255d692b0f15b35440e7f8687c846b55de
youssef abdmouleh <youssef.abdmouleh164@gmail.com>
2023-05-06 09:22:25 +0100

    last update

--- models/user.js
+++ models/user.js
@@ -1,21 +1,21 @@
-const mongoose =require("mongoose")
+const mongoose = require("mongoose")
 var userSchema = mongoose.Schema({
-nom:{
-type:String,
-required:"nom is required"
-} ,
-email:{
-type:String,
-required:"Email is required",
-unique:true
-} ,
-password:{
-type:String,
-required:"password is required"
-} ,
-role:{
-type:Number,
-default:0
-} ,
+    nom: {
+        type: String,
+        required: "nom is required"
+    },
+    email: {
+        type: String,
+        required: "Email is required",
+        unique: true
+    },
+    password: {
+        type: String,
+        required: "password is required"
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
 });
-module.exports=mongoose.model('User',userSchema)
+module.exports = mongoose.model('User', userSchema)
